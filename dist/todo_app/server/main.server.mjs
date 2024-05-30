de191c90ee0e234ffd8c3009179d3bdec701c05c
im410726 <o1.mirsaitov@gmail.com>
2024-05-30 18:29:05 +0600

    Third commit

---
+++ dist/todo_app/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-PPFFTGY5.mjs";import"./chunk-CLO6NECQ.mjs";import"./chunk-DSLC563T.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo_app' title`, () => {
+  it(`should have the 'task-list-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo_app');
+    expect(app.title).toEqual('task-list-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo_app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task-list-app');
   });
 });
